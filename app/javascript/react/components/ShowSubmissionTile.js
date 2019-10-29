c2777bac9d7711f6a21b64087b84b4f0be526118
DikPrice <richard@rosiep.clara.net>
2019-10-29 08:10:58 -0400

    Footer on showpage has different options for level 1 & 2 users including a transfer to master (function not yet implemented. Title bar shows users role if level 2

--- app/javascript/react/components/ShowSubmissionTile.js
+++ app/javascript/react/components/ShowSubmissionTile.js
@@ -5,18 +5,27 @@ const ShowSubmissionTile = props => {
     name, source, scale, description,
     length, width, height,
     first_name, last_name, phone, email,
+    created_at, updated_at,
     review
   } = props.submission
 
-  let edit, submitForReview
-  if (first_name == props.user["first_name"] && last_name == props.user["last_name"]){
+  let edit, submitForReview, showContacts, showTimestamps
+  if ((first_name == props.user["first_name"] && last_name == props.user["last_name"])
+      || (props.user["role"] === 2)){
     if (review === true){
       edit = "Model under review"
       submitForReview = ""
     } else {
     edit= <button className="button" onClick={props.edit}>Edit</button>
     submitForReview = <button className="button" onClick={props.forReview}>Submit</button>
+    showContacts = `Phone: ${phone}, Email: ${email}`
+    showTimestamps = <>Created: {created_at},<br />Updated: {updated_at}</>
     }
+  }
+
+  let transferToMaster
+  if (props.user["role"] === 2 && review === true) {
+    edit= <button className="button create-master" onClick={props.edit}>Create Master</button>
   }
 
   return (
@@ -36,16 +45,19 @@ const ShowSubmissionTile = props => {
       </div>
       <hr />
       <div className="rows meta">
-        <div className="columns small-12 large-10">
+        <div className="columns small-12 large-4">
           Length: {length}",
           Width: {width}",
           Height: {height}"<br />
-          Phone: {phone},
-          Email: {email}
+          {showContacts}
         </div>
-        <div className="columns small-12 large-2">
+        <div className="columns small-12 large-4">
+          {showTimestamps}
+        </div>
+        <div className="columns small-12 large-4">
           {submitForReview}
           {edit}
+          {transferToMaster}
         </div>
       </div>
     </div>
