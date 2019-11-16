d1a276f3a6ae486c0283848685367baf5b63d7a1
DikPrice <richard@rosiep.clara.net>
2019-11-16 12:11:45 -0500

    model & modeler examples added to factory_bot, model controller tests for index and show

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -26,5 +26,27 @@ FactoryBot.define do
     password { 'password' }
     password_confirmation { 'password' }
   end
+end
 
+FactoryBot.define do
+  factory :model do
+    name { "HMS Pinafore" }
+    scale { "1:700" }
+    source { "Kit" }
+    description { "We sail the ocean blue, And our saucy ship's a beauty; We're sober men and true, And attentive to our duty." }
+    length { 12 }
+    width { 8 }
+    height { 20 }
+    modeler_id { 1 }
+  end
+end
+
+FactoryBot.define do
+  factory :modeler do
+    first_name { 'Jeremiah' }
+    last_name { 'Pugwash' }
+    phone { "123 456 7890" }
+    email{ "hellosailor@saucy.com" }
+    user_id { 1 }
+  end
 end
